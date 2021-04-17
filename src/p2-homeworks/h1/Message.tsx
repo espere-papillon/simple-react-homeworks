ec983ed26e93df4847c8b0fa978291affc025197
espere-papillon <marinabov007@gmail.com>
2021-04-17 22:27:38 +0300

    feat: add message and alternative message with styles

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import stylesM from './Message.module.css'
 
-function Message() {
+type PropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: PropsType) {
     return (
-        <div>
-
+        <div className={stylesM.containerMessage}>
+            <img className={stylesM.avatar} src={props.avatar} alt="Ava"/>
+            <div className={stylesM.message}>
+                <h2 className={stylesM.nameMessage}>{props.name}</h2>
+                <h3 className={stylesM.textMessage}>{props.message}</h3>
+                <span className={stylesM.timeMessage}>{props.time}</span>
+            </div>
         </div>
     )
 }
