854a9b525e921cd3e712f942472c752fdf4e38e2
espere-papillon <marinabov007@gmail.com>
2021-06-21 20:20:10 +0300

    feat: update select and radio

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -1,10 +1,12 @@
 import React, {ChangeEvent, InputHTMLAttributes, DetailedHTMLProps} from 'react'
+import s from "./SuperRadio.module.css";
+
 
 type DefaultRadioPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperRadioPropsType = DefaultRadioPropsType & {
-    options?: any[]
-    onChangeOption?: (option: any) => void
+    options?: Array<string>
+    onChangeOption?: (option: string) => void
 }
 
 const SuperRadio: React.FC<SuperRadioPropsType> = (
@@ -12,19 +14,28 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
         type, name,
         options, value,
         onChange, onChangeOption,
+        className, title,
         ...restProps
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.title)
     }
 
+    const finalRadioClassName = `${s.input} ${className ? className : ''}` // need to fix with (?:) and s.superInput
 
     const mappedOptions: any[] = options ? options.map((o, i) => ( // map options with key
-        <label key={name + '-' + i}>
+        <label key={name + '-' + i} className={finalRadioClassName}>
             <input
                 type={'radio'}
                 // name, checked, value, onChange
+                name={name}
+                checked={o === value}
+                onChange={onChangeCallback}
+                title={o}
+                {...restProps}
             />
             {o}
         </label>
