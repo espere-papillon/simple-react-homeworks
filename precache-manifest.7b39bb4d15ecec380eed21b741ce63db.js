dbe4a972970916d4beb45b7d2cbc62999b613604
espere-papillon <marinabov007@gmail.com>
2021-06-25 00:50:16 +0300

    Updates

---
+++ precache-manifest.7b39bb4d15ecec380eed21b741ce63db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2350bf718760913af2387be711854245",
+    "url": "/simple-react-homeworks/index.html"
+  },
+  {
+    "revision": "b714821a7c6da5b031d1",
+    "url": "/simple-react-homeworks/static/css/main.703ed121.chunk.css"
+  },
+  {
+    "revision": "ecffbd80a4b8b38955f4",
+    "url": "/simple-react-homeworks/static/js/2.b5fd5ada.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/simple-react-homeworks/static/js/2.b5fd5ada.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b714821a7c6da5b031d1",
+    "url": "/simple-react-homeworks/static/js/main.0a0ec10f.chunk.js"
+  },
+  {
+    "revision": "af4ec8013c5fe87b14da",
+    "url": "/simple-react-homeworks/static/js/runtime-main.f59815ab.js"
+  },
+  {
+    "revision": "3092fb3defc87eef785e11e4c00a6d26",
+    "url": "/simple-react-homeworks/static/media/clipboard.3092fb3d.svg"
+  }
+]);
